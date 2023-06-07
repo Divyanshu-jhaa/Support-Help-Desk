b296f97b77a1d2735dfa6dc0c8c6fee7186449c0
divyanshu <divjha123@gmail.com>
2023-06-07 12:41:54 +0530

    Adding additional dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-
-
+import React from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import Login from "./pages/Login";
 function App() {
+  const location=useLocation();
   return (
+
     <>
-    
-    
+       <Routes>
+        <Route path="/" exact element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
     </>
   );
 }
